3abd2b0d8ed3ed767153c31ad35ded879c31dc1a
Mateus Cechella <113690168+CechellaMatt@users.noreply.github.com>
2022-10-28 08:11:44 -0300

    Exercício de Lista - CechellaMatt

---
+++ js/lista-CechellaMatt.js
@@ -0,0 +1,11 @@
+let hexa = ["Alisson", "Danilo", "Marquinhos", "G.Magalhães", "Emerson Royal", "Casemiro", "Paquetá", "Vini Jr", "Raphinha", "G.Jesus", "Neymar Jr"];
+console.log(hexa);
+
+hexa[7] = "Renan Lodi"; //Substituição Vini pelo Lodi
+console.log(hexa);
+
+hexa.shift(); //Alisson Expulso
+console.log(hexa);
+
+hexa[3] = "Wéverton";  //Substituição Emerson pelo Wéverton
+console.log(hexa);
